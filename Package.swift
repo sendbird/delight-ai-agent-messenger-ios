9a672ebac6cc707e3393c5e51cf4d3caffc8cd2f
Sendbird SDK Deployment <sha.sdk_deployment@sendbird.com>
2026-02-03 08:12:15 +0000

    Release - 1.5.0

--- Package.swift
+++ Package.swift
@@ -10,7 +10,7 @@ let package = Package(
     dependencies: [
         .package(
             url: "https://github.com/sendbird/delight-ai-agent-core-ios",
-            from: "1.4.3"
+            from: "1.5.0"
         )
     ],
     targets: [
